577a51a69c098fee6b9f7b825059e97872c5dc53
SnackyG <Klaus.e.gregersen@gmail.com>
2024-10-22 16:03:14 +0200

    changes

---
+++ src/main/java/com/example/learningcssproperties/LearningCssPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningcssproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCssPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningCssPropertiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningcssproperties/LearningCssPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningcssproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningCssPropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
